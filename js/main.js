20f85ba9de285c59d0d179e5ded3cde34c2ce635
pegazus-alpha <pourdebutantp@gmail.com>
2025-07-03 15:37:09 +0100

    deuxième commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-// AOS.init({
-//   duration: 800,
-//   once: true
-// });
+AOS.init({
+  duration: 800,
+  once: true
+});
+
 // // Exemple simple d'effet au scroll (optionnel)
-// window.addEventListener('scroll', function() {
-//   const header = document.querySelector('.hero');
-//   header.style.backgroundPositionY = -(window.scrollY * 0.3) + 'px';
-// });
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('.hero');
+  header.style.backgroundPositionY = -(window.scrollY * 0.3) + 'px';
+});
 // const sections = document.querySelectorAll('.section');
 
 // window.addEventListener('scroll', checkSections);
